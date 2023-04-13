c439798675075e39c049efe7afdfe2ac998ac085
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-04-13 14:28:51 +0200

    insert commit for backtick

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //use backtick for template
   template: ` <app-server></app-server>
     <app-server></app-server>`,
   styleUrls: ['./servers.component.scss'],
