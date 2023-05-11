e77865cf7fc60858befff4e05db533697d12dd4d
NB-WORK418\ulixe <amedeo.minozzi@eng.it>
2023-05-11 12:09:32 +0200

    esercizio per capire ngfor e toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  showDetail: boolean = false;
+  textPassword: string = 'SecretPassword';
+  dateArray: string[] = [];
+
+  public visualizza() {
+    let date = new Date();
+    this.dateArray.push(date.toISOString());
+    this.showDetail = true;
+    this.textPassword = 'tuna';
+  }
 }
